37a4cc56f7516240a4ad9f047ab871d8561c219f
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-09-22 15:25:51 +0200

    wire up everything to have a complete restore procedure

--- src/main/python/dynamodb_restore/__init__.py
+++ src/main/python/dynamodb_restore/__init__.py
@@ -1,18 +1,18 @@
 import json
 import tempfile
 import re
-
+import uuid
+import datetime
 import boto3
 
-dynamodb = boto3.client('dynamodb')
 
-def get_provisioned_throughput(parent):
+def _get_provisioned_throughput(table_definition):
     return {
-        "ReadCapacityUnits": parent["ProvisionedThroughput"]["ReadCapacityUnits"],
-        "WriteCapacityUnits": parent["ProvisionedThroughput"]["WriteCapacityUnits"]
+        "ReadCapacityUnits": table_definition["ProvisionedThroughput"]["ReadCapacityUnits"],
+        "WriteCapacityUnits": table_definition["ProvisionedThroughput"]["WriteCapacityUnits"]
     }
 
-def get_index(source_index):
+def _get_index(source_index):
     return {
         "IndexName": source_index["IndexName"],
         "KeySchema": source_index["KeySchema"],
@@ -20,20 +20,24 @@ def get_index(source_index):
     }
 
 
-def restore_schema(target_table_name, table_definition):
+def restore_schema(table_definition, region, target_table_name=None):
+
+    dynamodb = boto3.client('dynamodb', region_name=region)
+    table_name = target_table_name if target_table_name else table_definition["TableName"]
+
     create_table_args = {
-        "TableName": target_table_name,
+        "TableName": table_name,
         "AttributeDefinitions": table_definition['AttributeDefinitions'],
         "KeySchema": table_definition['KeySchema'],
-        "ProvisionedThroughput": get_provisioned_throughput(table_definition)
+        "ProvisionedThroughput": _get_provisioned_throughput(table_definition)
     }
 
     global_secondary_index_definitions = table_definition.get('GlobalSecondaryIndexes', [])
     if global_secondary_index_definitions:
         global_secondary_indexes = []
         for source_index in global_secondary_index_definitions:
-            target_index = get_index(source_index)
-            target_index["ProvisionedThroughput"] = get_provisioned_throughput(source_index)
+            target_index = _get_index(source_index)
+            target_index["ProvisionedThroughput"] = _get_provisioned_throughput(source_index)
             global_secondary_indexes.append(target_index)
 
         create_table_args['GlobalSecondaryIndexes'] = global_secondary_indexes
@@ -42,7 +46,7 @@ def restore_schema(target_table_name, table_definition):
     if local_secondary_index_definitions:
         local_secondary_indexes = []
         for source_index in local_secondary_index_definitions:
-            local_secondary_indexes.append(get_index(source_index))
+            local_secondary_indexes.append(_get_index(source_index))
 
         create_table_args['LocalSecondaryIndexes'] = local_secondary_indexes
 
@@ -51,35 +55,48 @@ def restore_schema(target_table_name, table_definition):
 
     dynamodb.create_table(**create_table_args)
 
-    dynamodb.get_waiter("table_exists").wait(TableName=target_table_name)
+    dynamodb.get_waiter("table_exists").wait(TableName=table_name)
+
+    return table_name
 
 
-def load_schema(schema_location, table_name):
-    m = re.match("s3://([\w-]+)/(.*)", schema_location)
+def load_schema(schema_location):
+    return load_json_file(schema_location)['Table']
+
+
+def load_json_file(location):
+    m = re.match("s3://([\w-]+)/(.*)", location)
     if m:
         s3 = boto3.client("s3")
         bucket = m.group(1)
         object_name = m.group(2)
-        local_file = tempfile.gettempdir() + "/" + table_name + "-schema.json"
+        local_file = tempfile.gettempdir() + "/" + str(uuid.uuid4()) + ".json"
         s3.download_file(bucket, object_name, local_file)
     else:
-        local_file = schema_location
+        local_file = location
 
     with open(local_file, "r") as f:
-        return json.loads(f.read())['Table']
+        return json.load(f)
 
 
-def create_datapipeline(definition, subnet_id, ddb_table_name, s3_loc):
-    _pipeline_name = "restore_" + ddb_table_name
-    with open(definition) as definition_file:
-        pipeline_objects = json.load(definition_file)
+def create_datapipeline(definition, subnet_id, ddb_table_name, s3_loc, region):
+    pipeline_name = "restore_" + ddb_table_name
+    pipeline_template = load_json_file(definition)
 
     parameter_values = [{"id": "mySubnetId", "stringValue": subnet_id},
                         {"id": "myDDBTableName", "stringValue": ddb_table_name},
                         {"id": "myInputS3Loc", "stringValue": s3_loc}]
 
-    client = boto3.client("datapipeline", region_name="eu-west-1")
-    pipeline = client.create_pipeline(name=_pipeline_name, uniqueId=_pipeline_name)
-    client.put_pipeline_definition(pipelineId=pipeline["pipelineId"],
-                                   pipelineObjects=pipeline_objects["objects"],
+    client = boto3.client("datapipeline", region_name=region)
+    pipeline = client.create_pipeline(name=pipeline_name, uniqueId=pipeline_name)
+    pipeline_id = pipeline["pipelineId"]
+
+    client.put_pipeline_definition(pipelineId=pipeline_id,
+                                   pipelineObjects=pipeline_template["pipelineObjects"],
+                                   parameterObjects=pipeline_template["parameterObjects"],
                                    parameterValues=parameter_values)
+
+    client.activate_pipeline(
+        pipelineId=pipeline_id,
+        startTimestamp=datetime.datetime.utcnow()
+    )
--- src/main/scripts/dynamodb-restore.py
+++ src/main/scripts/dynamodb-restore.py
@@ -4,22 +4,28 @@
 dynamodb-restore.py
 
 Usage:
-  dynamodb-restore.py --pipeline-definition-uri=pipeline_uri --subnetId=subnetId --ddbtablename=targetTableName --backup-source=s3_uri --schema-definition=local_file_or_s3_uri
+  dynamodb-restore.py [--pipeline-definition-uri=pipeline_uri] --subnetId=subnetId [--tablename=targetTableName] --backup-source=s3_uri --schema-definition=local_file_or_s3_uri [--region=region]
 
 Options:
-  -h --help     Show this screen.
+  -h --help  Show this screen.
+  --pipeline-definition-uri=url  Restore DataPipeline template URL  [default: s3://is24-cfn-templates/infrastructure/restore-dynamodb-from-s3.datapipeline.json]
+  --region=region  Region to create DynamoDB Table and DataPipeline Job in  [default: eu-west-1]
 """
 
 from docopt import docopt
 from dynamodb_restore import restore_schema, create_datapipeline, load_schema
 
-#args = docopt(__doc__)
-#print(args)
-#table_name = args["--ddbtablename"]
+args = docopt(__doc__)
+table_name = args["--tablename"]
+region = args["--region"]
 
-table_definition = load_schema("/var/folders/lj/6l98gzws3mjcypf6j4p4w0zc0017ft/T/bla-schema.json", "bla")
+table_definition = load_schema(args["--schema-definition"])
 
-print table_definition
-#restore_schema(table_name)
+print "Restoring schema"
+restored_table_name = restore_schema(table_definition, region, table_name)
 
-#create_datapipeline(args["--pipeline-definition-uri"], args["--subnetId"], table_name, args["--backup-source"])
+print "Creating datapipeline"
+create_datapipeline(args["--pipeline-definition-uri"], args["--subnetId"], restored_table_name, args["--backup-source"],
+                    region)
+
+print "Restore triggered successfully!"
